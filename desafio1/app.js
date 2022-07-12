01ef98791ff5e9dd5473123ec7617e786dde0d8d
caiquecampanaro <caiquecampanaro@gmail.com>
2022-07-12 15:08:15 -0300

    atualização

---
+++ desafio1/app.js
@@ -0,0 +1,6 @@
+"use strict";
+//Desafio 1: arrumar o codigo para que ele possa rodar.
+let funcionario = {
+    codigo: 10,
+    nome: 'John'
+};
---
+++ desafio1/app.ts
@@ -0,0 +1,15 @@
+//Desafio 1: arrumar o codigo para que ele possa rodar.
+
+// let funcionario = {};
+// funcionario.codigo = 10;
+// funcionario.nome = "John";
+
+interface Funcionario {
+  codigo: number,
+  nome: string
+}
+
+let funcionario: Funcionario = {
+  codigo: 10,
+  nome: 'John'
+}
